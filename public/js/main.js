90b18fc51c9bc351dbd2a0415e7890b686886199
Salahudin Al Ayubi <salahudinalyb@gmail.com>
2024-01-14 10:43:24 +0700

    Bottom Nav Added 🍉

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,6 @@ function playPauseToggle() {
       icon.attr("src","../src/icon/muted.svg");
     }
   };
+
+ // TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
+const mySmoothScroll = new SmoothScroll(document.getElementById('smoothScroll'), options);
