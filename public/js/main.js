d4e46ac19ae6eaed7f1b6211db9a7e62c64507f3
Salahudin Al Ayubi <salahudinalyb@gmail.com>
2024-01-14 11:28:00 +0700

    Deleting Unnecessary Line 🍓

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,3 @@ function playPauseToggle() {
       icon.attr("src","../src/icon/muted.svg");
     }
   };
-
- // TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
-const mySmoothScroll = new SmoothScroll(document.getElementById('smoothScroll'), options);
